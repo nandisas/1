f688803ae9c876294c4139ebf815203fe7e687e8
nandisas <nandi.tubai007@gmail.com>
2020-11-12 17:22:21 +0530

    Updates

---
+++ precache-manifest.90652058295be6e1cfb03d39c154850a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7dd4cdb8a87f33676d5575363ba4c4",
+    "url": "/1/index.html"
+  },
+  {
+    "revision": "a750ed66101e05485f2f",
+    "url": "/1/static/css/2.d741556c.chunk.css"
+  },
+  {
+    "revision": "b9a79a6d949b7db29c51",
+    "url": "/1/static/css/main.581ab7dc.chunk.css"
+  },
+  {
+    "revision": "a750ed66101e05485f2f",
+    "url": "/1/static/js/2.c68549a2.chunk.js"
+  },
+  {
+    "revision": "3c61ede094c3615ba32e9288f20686bf",
+    "url": "/1/static/js/2.c68549a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a79a6d949b7db29c51",
+    "url": "/1/static/js/main.449a7e38.chunk.js"
+  },
+  {
+    "revision": "1b81e829a2fde4f861b2",
+    "url": "/1/static/js/runtime-main.6d48c9b7.js"
+  }
+]);
